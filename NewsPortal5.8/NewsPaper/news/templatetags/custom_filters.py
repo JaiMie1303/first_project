83f8879f71adb58b4cbe495729b74b3ce0b513d2
JaiMie1303 <evgeniyasmirnova89@gmail.com>
2023-07-27 15:53:42 +0300

    NP5.8

---
+++ NewsPortal5.8/NewsPaper/news/templatetags/custom_filters.py
@@ -0,0 +1,22 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter()
+def censor(value):
+    if not isinstance(value, str):
+        raise ValueError('Должен быть текст!')
+
+    nasty_list = [
+        'редиска',
+        'жук',
+        'мякиш',
+        'лунтик',
+    ]
+
+    for item in nasty_list:
+        if item in value.lower():
+            c_index = value.lower().index(item)
+            value = value.replace(value[c_index:c_index+len(item)], f"{value[c_index]}{'*' * (len(item) - 1)}")
+    return value
